5d51c2841bc1c205199a0fb79055122615ab6bca
The Cheaterman <the.cheaterman@gmail.com>
2023-07-01 15:26:58 +0200

    Add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
+import pathlib
+import sys
+
+from sphinx_pyproject import SphinxConfig
+
+config = SphinxConfig('../../pyproject.toml', globalns=globals())
+sys.path.insert(0, pathlib.Path('../../samp_query').resolve())
+current_year = datetime.date.today().year
+
+project, release = name, version
+copyright = f'{current_year}, {author}'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
--- samp_query/__init__.py
+++ samp_query/__init__.py
@@ -11,6 +11,14 @@ MAX_LATENCY_VARIANCE = 5
 
 
 def encode_codepage(string: str) -> bytes:
+    """
+    Encode the given string into bytes using the first possible codepage.
+
+    :param str string: The string to encode.
+    :return: The encoded bytes.
+    :rtype: bytes
+    :raises UnicodeEncodeError: If no suitable codepage is found.
+    """
     for codepage in range(1250, 1259):
         try:
             return string.encode(f'cp{codepage}')
@@ -27,11 +35,27 @@ def encode_codepage(string: str) -> bytes:
 
 
 def pack_string(string: str, len_type: str) -> bytes:
+    """
+    Pack a string into bytes with a length prefix.
+
+    :param str string: The string to pack.
+    :param str len_type: The format specifier for the length prefix.
+    :return: The packed bytes.
+    :rtype: bytes
+    """
     format = f'<{len_type}'
     return struct.pack(format, len(string)) + encode_codepage(string)
 
 
 def unpack_string(data: bytes, len_type: str) -> tuple[str, bytes]:
+    """
+    Unpack a string from bytes with a length prefix.
+
+    :param bytes data: The data to unpack.
+    :param str len_type: The format specifier for the length prefix.
+    :return: The unpacked string and the remaining data.
+    :rtype: tuple[str, bytes]
+    """
     format = f'<{len_type}'
     size = struct.calcsize(format)
     str_len, data = *struct.unpack_from(format, data), data[size:]
@@ -41,19 +65,30 @@ def unpack_string(data: bytes, len_type: str) -> tuple[str, bytes]:
 
 
 class MissingRCONPassword(Exception):
-    pass
+    """Raised when no RCON password was provided."""
 
 
 class InvalidRCONPassword(Exception):
-    pass
+    """Raised when an invalid RCON password is provided."""
 
 
 class RCONDisabled(Exception):
-    pass
+    """Raised when RCON is disabled on the server or did not respond."""
 
 
 @dataclass
 class ServerInfo:
+    """
+    Represents server information.
+
+    :param str name: The name of the server.
+    :param bool password: Indicates if the server requires a password to join.
+    :param int players: The number of players on the server.
+    :param int max_players:
+        The maximum number of players allowed on the server.
+    :param str gamemode: The current gamemode of the server.
+    :param str language: The language used by the server.
+    """
     name: str
     password: bool
     players: int
@@ -63,6 +98,13 @@ class ServerInfo:
 
     @classmethod
     def from_data(cls, data: bytes) -> ServerInfo:
+        """
+        Create a ServerInfo object from the given raw data.
+
+        :param bytes data: The data to create the ServerInfo object from.
+        :return: The created ServerInfo object.
+        :rtype: ServerInfo
+        """
         password, players, max_players = struct.unpack_from('<?HH', data)
         data = data[5:]  # _Bool + short + short, see above
         name, data = unpack_string(data, 'I')
@@ -83,11 +125,24 @@ class ServerInfo:
 
 @dataclass
 class PlayerInfo:
+    """
+    Represents player information.
+
+    :param str name: The name of the player.
+    :param int score: The score of the player.
+    """
     name: str
     score: int
 
     @classmethod
     def from_data(cls, data: bytes) -> tuple[PlayerInfo, bytes]:
+        """
+        Create a PlayerInfo object from the given raw data.
+
+        :param bytes data: The data to create the PlayerInfo object from.
+        :return: The created PlayerInfo object and the remaining data.
+        :rtype: tuple[PlayerInfo, bytes]
+        """
         name, data = unpack_string(data, 'B')
         score = struct.unpack_from('<i', data)[0]
         data = data[4:]  # int, see above
@@ -100,10 +155,22 @@ class PlayerInfo:
 
 @dataclass
 class PlayerList:
+    """
+    Represents a list of players.
+
+    :param list[PlayerInfo] players: The list of players.
+    """
     players: list[PlayerInfo]
 
     @classmethod
     def from_data(cls, data: bytes) -> PlayerList:
+        """
+        Create a PlayerList object from the given raw data.
+
+        :param bytes data: The data to create the PlayerList object from.
+        :return: The created PlayerList object.
+        :rtype: PlayerList
+        """
         player_count = struct.unpack_from('<H', data)[0]
         data = data[2:]  # short, see above
         players = []
@@ -119,11 +186,24 @@ class PlayerList:
 
 @dataclass
 class Rule:
+    """
+    Represents a server rule.
+
+    :param str name: The name of the rule.
+    :param str value: The value of the rule.
+    """
     name: str
     value: str
 
     @classmethod
     def from_data(cls, data: bytes) -> tuple[Rule, bytes]:
+        """
+        Create a Rule object from the given raw data.
+
+        :param bytes data: The data to create the Rule object from.
+        :return: The created Rule object and the remaining data.
+        :rtype: tuple[Rule, bytes]
+        """
         name, data = unpack_string(data, 'B')
         value, data = unpack_string(data, 'B')
 
@@ -135,10 +215,22 @@ class Rule:
 
 @dataclass
 class RuleList:
+    """
+    Represents a list of server rules.
+
+    :param list[Rule] rules: The list of rules.
+    """
     rules: list[Rule]
 
     @classmethod
     def from_data(cls, data: bytes) -> RuleList:
+        """
+        Create a RuleList object from the given raw data.
+
+        :param bytes data: The data to create the RuleList object from.
+        :return: The created RuleList object.
+        :rtype: RuleList
+        """
         rule_count = struct.unpack_from('<H', data)[0]
         data = data[2:]  # short, see above
         rules = []
@@ -154,6 +246,13 @@ class RuleList:
 
 @dataclass
 class Client:
+    """
+    Main query client class to interact with a given game server.
+
+    :param str ip: The IP address of the server.
+    :param int port: The port number of the server.
+    :param str | None rcon_password: The RCON password for the server (optional).
+    """
     ip: str
     port: int
     rcon_password: str | None = field(default=None, repr=False)
@@ -162,6 +261,7 @@ class Client:
     _socket: trio.socket.SocketType | None = field(default=None, repr=False)
 
     async def connect(self) -> None:
+        """Connect to the server (called automatically)."""
         family, type, proto, _, (ip, *_) = (await trio.socket.getaddrinfo(
             self.ip,
             self.port,
@@ -178,6 +278,12 @@ class Client:
         )
 
     async def send(self, opcode: bytes, payload: bytes = b'') -> None:
+        """
+        Send a query message to the server.
+
+        :param bytes opcode: The opcode of the message.
+        :param bytes payload: The payload of the message (optional).
+        """
         if not self._socket:
             await self.connect()
 
@@ -185,6 +291,13 @@ class Client:
         await self._socket.send(self.prefix + opcode + payload)
 
     async def receive(self, header: bytes = b'') -> bytes:
+        """
+        Receive a query response from the server.
+
+        :param bytes header: The expected header of the response (optional).
+        :return: The received response.
+        :rtype: bytes
+        """
         assert self._socket
 
         while True:
@@ -194,6 +307,12 @@ class Client:
                 return data[len(header):]
 
     async def ping(self) -> float:
+        """
+        Send a ping request to the server and measure the round-trip time.
+
+        :return: The round-trip time in seconds.
+        :rtype: float
+        """
         payload = random.getrandbits(32).to_bytes(4, 'little')
         start_time = trio.current_time()
         await self.send(b'p', payload)
@@ -203,6 +322,12 @@ class Client:
         return trio.current_time() - start_time
 
     async def is_omp(self) -> bool:
+        """
+        Check if the server uses open.mp.
+
+        :return: True if the server uses open.mp, False otherwise.
+        :rtype: bool
+        """
         ping = await self.ping()
         payload = random.getrandbits(32).to_bytes(4, 'little')
 
@@ -216,24 +341,53 @@ class Client:
         return False
 
     async def info(self) -> ServerInfo:
+        """
+        Retrieve server information.
+
+        :return: The server information.
+        :rtype: ServerInfo
+        """
         await self.send(b'i')
         assert self.prefix
         data = await self.receive(header=self.prefix + b'i')
         return ServerInfo.from_data(data)
 
     async def players(self) -> PlayerList:
+        """
+        Retrieve the list of players on the server.
+
+        :return: The list of players.
+        :rtype: PlayerList
+        """
         await self.send(b'c')
         assert self.prefix
         data = await self.receive(header=self.prefix + b'c')
         return PlayerList.from_data(data)
 
     async def rules(self) -> RuleList:
+        """
+        Retrieve the list of server rules.
+
+        :return: The list of rules.
+        :rtype: RuleList
+        """
         await self.send(b'r')
         assert self.prefix
         data = await self.receive(header=self.prefix + b'r')
         return RuleList.from_data(data)
 
     async def rcon(self, command: str) -> str:
+        """
+        Execute a RCON command on the server.
+
+        :param str command: The RCON command to execute.
+        :return: The response from the server.
+        :rtype: str
+        :raises MissingRCONPassword: If the RCON password is missing.
+        :raises InvalidRCONPassword: If an invalid RCON password is provided.
+        :raises RCONDisabled:
+            If RCON is disabled on the server or no response was received.
+        """
         if not self.rcon_password:
             raise MissingRCONPassword()
 
